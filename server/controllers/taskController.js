2cc9e021a655ac14ad2c2e82a2b98d8cdada64c2
007codename <juri.malaj@gmail.com>
2026-09-13 23:04:51 +0200

    feat: include User data when fetching tasks

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,5 +1,5 @@
 // server/controllers/taskController.js
-const { Task } = require("../models");
+const { Task, User } = require("../models");
 
 // CREATE — POST /
 async function createTask(req, res) {
@@ -14,7 +14,7 @@ async function createTask(req, res) {
 // READ ALL — GET /
 async function getAllTasks(req, res) {
   try {
-    const tasks = await Task.findAll();
+    const tasks = await Task.findAll({ include: User });
     res.status(200).json(tasks);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -24,7 +24,7 @@ async function getAllTasks(req, res) {
 // READ ONE — GET /:id
 async function getTaskById(req, res) {
   try {
-    const task = await Task.findByPk(req.params.id);
+    const task = await Task.findByPk(req.params.id, { include: User });
     if (!task) {
       return res.status(404).json({ error: "Task not found" });
     }
