5173ef09455fc1f2fb968348aa3d96714de02e99
007codename <juri.malaj@gmail.com>
2026-09-14 12:54:41 +0200

    feat: mount user routes in app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());          // allows requests from your React dev server (differe
 app.use(express.json());  // parses JSON request bodies into req.body
 
 app.use('/api/tasks', taskRoutes); // all routes above are now prefixed with /api/tasks
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
